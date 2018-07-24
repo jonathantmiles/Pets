8c842d3ba0aab83680f49beeb34d389496f236c1
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-07-24 16:02:30 -0700

    Initial commit

---
+++ Pets/PetsTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PetsTableViewController.swift
+//  Pets
+//
+//  Created by Jonathan T. Miles on 7/24/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+import UIKit
+
+class PetsTableViewController: UITableViewController {
+
+    let array = ["Fancy", "Phantom", "Fluffy"]
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return array.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PetCell", for: indexPath)
+
+        // Configure the cell...
+
+        let thisPet = array[indexPath.row]
+        cell.textLabel?.text = thisPet
+        
+        return cell
+    }
+    
+
+}
